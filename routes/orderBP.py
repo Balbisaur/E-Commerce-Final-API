965c03fa74cb1fc5a8b598dc33cb4b927570b64a
Gerardo Ramirez <166074441+Balbisaur@users.noreply.github.com>
2024-07-30 07:01:19 -0500

    Update orderBP.py

--- routes/orderBP.py
+++ routes/orderBP.py
@@ -1,10 +1,10 @@
 from flask import Blueprint
 from controllers.orderController import find_by_customer_email, find_by_customer_id, find_by_id, save, find_all
 
-# Create a Blueprint for order-related routes
+# Creating a Blueprint for order-related routes
 order_blueprint = Blueprint('order_bp', __name__)
 
-# Define routes and their corresponding view functions
+# Defining routes and their corresponding view functions
 order_blueprint.add_url_rule('/', methods=['POST'], view_func=save)
 order_blueprint.add_url_rule('/', methods=['GET'], view_func=find_all)
 order_blueprint.add_url_rule('/<int:id>', methods=['GET'], view_func=find_by_id)
