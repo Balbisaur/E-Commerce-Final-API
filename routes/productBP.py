a6d57e23ab7fd39e66ded70a4276bab2f8ee6147
Gerardo Ramirez <166074441+Balbisaur@users.noreply.github.com>
2024-07-30 07:01:46 -0500

    Update productBP.py

--- routes/productBP.py
+++ routes/productBP.py
@@ -1,10 +1,10 @@
 from flask import Blueprint
 from controllers.productController import find_all, save, search_product
 
-# Create a Blueprint for product-related routes
+# Creating a Blueprint for product-related routes
 product_blueprint = Blueprint('product_bp', __name__)
 
-# Define routes and their corresponding view functions
+# Defining routes and their corresponding view functions
 product_blueprint.add_url_rule('/', methods=['POST'], view_func=save)
 product_blueprint.add_url_rule('/', methods=['GET'], view_func=find_all)
 product_blueprint.add_url_rule('/search', methods=['GET'], view_func=search_product)
