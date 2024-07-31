e01dcea3f5f86a7fe6f4f3fcdafe4893c3d4d162
Gerardo Ramirez <166074441+Balbisaur@users.noreply.github.com>
2024-07-31 14:48:23 -0500

    Update productService.py

--- services/productService.py
+++ services/productService.py
@@ -3,18 +3,32 @@ from database import db
 from sqlalchemy import select
 
 def save(product_data):
+    # I am creating a new Product instance with the provided data
     new_product = Product(name=product_data['name'], price=product_data['price'])
+    
+    # Adding the new product to the session and commit
     db.session.add(new_product)
     db.session.commit()
+    
+    # Refresh the session to get the latest state of the new product
     db.session.refresh(new_product)
+    
     return new_product
 
 def find_all():
+    # Retrieving all products
     query = select(Product)
     all_products = db.session.execute(query).scalars().all()
     return all_products
 
+def find_all_paginated():
+    # Retrieving all products paginated
+    query = select(Product)
+    products = db.paginate(query, page=1, per_page=10)
+    return products
+
 def search_product(search_term):
+    # Searching for products where the name contains the search term
     query = select(Product).where(Product.name.ilike(f'%{search_term}%'))
     search_products = db.session.execute(query).scalars().all()
     return search_products
